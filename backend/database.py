c3b82d768fd676841a1d7017c65da5bc257ddc24
Manas Kumar <singhmanas691@gmail.com>
2026-09-06 21:21:34 +0530

    backend fix

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
+import os
 import sqlite3
 
-def get_db_connection():
-    connection = sqlite3.connect("cinematch.db")
-    return connection
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cinematch.db")
 
-    
+def get_db_connection():
+    connection = sqlite3.connect(DB_PATH)
+    return connection
